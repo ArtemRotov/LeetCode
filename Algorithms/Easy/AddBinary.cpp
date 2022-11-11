60d96308f464c3993381fb2cc94d415b51566578
artem.rotov <artem.rotov@vniira.spb.su>
2022-11-11 14:59:05 +0300

    New task

---
+++ Algorithms/Easy/AddBinary.cpp
@@ -0,0 +1,22 @@
+std::string addBinary(std::string a, std::string b)
+{
+    std::string result;
+
+    short i = a.length() - 1;
+    short j = b.length() - 1;
+
+    int sum = 0;
+
+    for ( ; i >= 0 || j >= 0 || sum; --i, --j)
+    {
+        sum = (i >= 0) ? sum + (a[i] - '0') : sum;
+        sum = (j >= 0) ? sum + (b[j] - '0') : sum;
+
+        result.append(std::to_string(sum % 2));
+        sum = sum / 2;
+    }
+
+    std::reverse(result.begin(),result.end());
+
+    return result;
+}
