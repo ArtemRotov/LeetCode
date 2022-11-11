6079725968d3f976f58a2087e1a303f7f81319b8
artem.rotov <artem.rotov@vniira.spb.su>
2022-11-11 15:00:48 +0300

    New task

--- Algorithms/Easy/LengthOfLastWord.cpp
+++ Algorithms/Easy/LengthOfLastWord.cpp
@@ -1,5 +1,5 @@
 int lengthOfLastWord(std::string s)
-    {
-        short lastChar = s.find_last_not_of(" ");
-        return (lastChar - s.rfind(" ",lastChar));
-    }
+{
+    short lastChar = s.find_last_not_of(" ");
+    return (lastChar - s.rfind(" ",lastChar));
+}
