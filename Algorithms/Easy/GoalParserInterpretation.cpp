b70323610fd28ed5845411e0fe3f3d182065ec3d
artem.rotov <rotoffff@yandex.ru>
2022-11-23 11:22:01 +0300

    New task

---
+++ Algorithms/Easy/GoalParserInterpretation.cpp
@@ -0,0 +1,25 @@
+std::string interpret(std::string command)
+{
+    std::string result = "";
+
+    for (int i = 0; i < command.length();)
+    {
+        if (command[i] == 'G')
+        {
+            result += "G";
+            i++;
+        }
+        else if (command[i] == '(' && command[i+1] == ')')
+        {
+            result += "o";
+            i += 2;
+        }
+        else
+        {
+            result += "al";
+            i += 4;
+        }
+    }
+
+    return result;
+}
