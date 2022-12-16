7ff448289dbede5c8b9bb42e10e91ab4a34071f1
artem.rotov <rotoffff@yandex.ru>
2022-12-16 19:38:59 +0300

    second solution

--- Algorithms/Easy/FlippingAnImage.cpp
+++ Algorithms/Easy/FlippingAnImage.cpp
@@ -1,4 +1,4 @@
-// Without stl algorithms (except swap)
+// With only STL swap algorithm
 std::vector<std::vector<int>> flipAndInvertImage(std::vector<std::vector<int>>& image)
 {
     for (int i = 0; i < image.size(); ++i)
@@ -11,6 +11,19 @@ std::vector<std::vector<int>> flipAndInvertImage(std::vector<std::vector<int>>& 
             std::swap(image[i][j],image[i][n - 1 - j]);
         }
         if (n % 2 == 1)   image[i][n/2] = (image[i][n/2] + 1) % 2;
+    }
+
+    return image;
+}
+
+// With STL reverse algorithm
+std::vector<std::vector<int>> flipAndInvertImage(std::vector<std::vector<int>>& image)
+{
+    for (auto &row : image)
+    {
+        std::reverse(row.begin(), row.end());
+
+        for (auto &el : row) el ^= 1;
     }
 
     return image;
