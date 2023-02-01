4511600ce3d697258e400c9ca6cab6cf48c477ac
artem.rotov <rotoffff@yandex.ru>
2023-02-01 11:27:04 +0300

    feet: new sql task

---
+++ Database/Medium/SecondHighestSalary.sql
@@ -0,0 +1,22 @@
+/*
+176. Second Highest Salary
+
+Table: Employee
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write an SQL query to report the second highest salary from the Employee table.
+If there is no second highest salary, the query should report null.
+*/
+
+# Write your MySQL query statement below
+
+SELECT MAX(E.salary) AS SecondHighestSalary FROM Employee E
+WHERE E.salary <> (SELECT MAX(salary) FROM Employee)
