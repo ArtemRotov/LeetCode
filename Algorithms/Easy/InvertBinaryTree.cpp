55017113aa591b4d42354d416a91633962f0f009
artem.rotov <rotoffff@yandex.ru>
2023-03-29 23:16:19 +0300

    feat: new task

---
+++ Algorithms/Easy/InvertBinaryTree.cpp
@@ -0,0 +1,31 @@
+/*
+226. Invert Binary Tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) 
+    {
+        if (!root)    
+            return nullptr;
+        
+        TreeNode* tmp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(tmp);
+
+        return root;
+    }
+};
