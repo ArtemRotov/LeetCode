4ca15b7b4ba66899651cf9e11555356c56f828f7
Artyom Rotov <rotoffff@yandex.ru>
2023-04-11 13:51:35 +0300

    feat: new solution

--- Algorithms/Medium/Permutations.cpp
+++ Algorithms/Medium/Permutations.cpp
@@ -6,29 +6,57 @@ You can return the answer in any order.
 */
 
 class Solution {
+    std::vector<std::vector<int>> res{};
 public:
-    void permute(std::vector<std::vector<int>> &result, std::vector<int> &nums, int i)
+    std::vector<std::vector<int>> permute(std::vector<int>& nums) 
     {
-        if (i == nums.size())
+        helper(nums, 0);
+        return res;
+    }
+
+    void helper(std::vector<int>& nums, int idx)
+    {
+        if (idx == nums.size())
         {
-            result.push_back(nums);
+            res.push_back(nums);
             return;
-        } 
-        
-        for (int j = i; j < nums.size(); j++) 
+        }
+
+        for (int i = idx; i < nums.size(); i++)
         {
-            std::swap(nums[i], nums[j]);
-            permute(result, nums, i + 1);
-            std::swap(nums[i], nums[j]);
+            std::swap(nums[i], nums[idx]);
+            helper(nums, idx + 1);
+            std::swap(nums[i], nums[idx]);
         }
     }
     
-    std::vector<std::vector<int>> permute(std::vector<int>& nums) 
-    {
-        std::vector<std::vector<int>> result;
-        permute(result, nums, 0);
-        return result;
-    }
-};
+    // std::vector<std::vector<int>> permute(std::vector<int>& nums) 
+    // {
+    //     std::vector<int> v{};
+    //     std::unordered_map<int,int> idx{};
+    //     helper(nums,v,idx);
+    //     return res;
+    // }
 
+    // void helper(std::vector<int>& nums, std::vector<int>& v, std::unordered_map<int,int>& idx)
+    // {
+    //     if (v.size() == nums.size())
+    //     {
+    //         res.push_back(v);
+    //         return;
+    //     }
+        
+    //     for (int i = 0; i < nums.size(); i++)
+    //     {
+    //         if (idx[i])
+    //             continue;
+            
+    //         v.push_back(nums[i]);
+    //         idx[i]++;
+    //         helper(nums, v, idx);
+    //         v.pop_back();
+    //         idx[i]--;
+    //     }
+    // }
+};
 
