9b687c6ab2f20d64953024447f50a4789742b503
Artyom Rotov <rotoffff@yandex.ru>
2023-04-24 13:03:37 +0300

    feat: go sol

---
+++ Algorithms/Easy/PathSum.go
@@ -0,0 +1,30 @@
+/*
+112. Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true 
+if the tree has a root-to-leaf path such that adding up all the values 
+along the path equals targetSum.
+
+A leaf is a node with no children.
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func hasPathSum(root *TreeNode, targetSum int) bool {
+
+    if (root == nil) {
+        return false;
+    }
+    if (root.Val == targetSum && root.Left == nil && root.Right == nil) {
+        return true;
+    }
+
+    return hasPathSum(root.Left, targetSum - root.Val) || hasPathSum(root.Right, targetSum - root.Val);
+    
+}
