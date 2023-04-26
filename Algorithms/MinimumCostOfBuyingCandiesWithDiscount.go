1ccba78e6443024d54f99d9bbd6197ee0ee01360
Artyom Rotov <rotoffff@yandex.ru>
2023-04-26 11:47:49 +0300

    feat: new task

---
+++ Algorithms/MinimumCostOfBuyingCandiesWithDiscount.go
@@ -0,0 +1,28 @@
+/*2144. Minimum Cost of Buying Candies With Discount
+A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free.
+
+The customer can choose any candy to take away for free as long as the cost of the chosen candy is less 
+than or equal to the minimum cost of the two candies bought.
+
+For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 
+2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4.
+
+Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum 
+cost of buying all the candies.
+*/
+
+func minimumCost(cost []int) int {
+    sort.Ints(cost)
+    sum := 0
+    cnt := 0
+    for i := len(cost) - 1; i >= 0; i-- {
+        if cnt == 2 {
+            cnt = 0
+            continue
+        }
+        sum += cost[i]
+        cnt++
+    }
+
+    return sum
+}
