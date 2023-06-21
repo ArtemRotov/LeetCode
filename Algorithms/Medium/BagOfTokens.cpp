5e80711e5baeefc2e3722cc3d90d0bfdaa6fad2f
Artyom Rotov <rotoffff@yandex.ru>
2023-06-21 17:09:16 +0300

    feat: new task

---
+++ Algorithms/Medium/BagOfTokens.cpp
@@ -0,0 +1,40 @@
+/*
+948. Bag of Tokens
+
+You have an initial power of power, an initial score of 0, and a bag of tokens where tokens[i] is the value of the ith token (0-indexed).
+
+Your goal is to maximize your total score by potentially playing each token in one of two ways:
+
+If your current power is at least tokens[i], you may play the ith token face up, losing tokens[i] power and gaining 1 score.
+If your current score is at least 1, you may play the ith token face down, gaining tokens[i] power and losing 1 score.
+Each token may be played at most once and in any order. You do not have to play all the tokens.
+
+Return the largest possible score you can achieve after playing any number of tokens.
+*/
+
+
+class Solution {
+public:
+    int bagOfTokensScore(std::vector<int>& tokens, int power) 
+    {
+        std::sort(tokens.begin(), tokens.end());
+        int result = 0;
+        int score = 0;
+        for (int i = 0, j = tokens.size() - 1; i <= j;)
+        {
+            if (power >= tokens[i])
+            {
+                power -= tokens[i++];
+                result = std::max(result, ++score);
+            }
+            else if (score > 0)
+            {
+                score--;
+                power += tokens[j--];
+            }
+            else break;
+        }
+
+        return result;
+    }
+};
